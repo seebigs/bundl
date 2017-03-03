bb2c9e123de5c40a77b311e3d4dc1faf69f052a4
Chris Bigelow <seebigs@gmail.com>
2017-03-03 14:15:31 -0500

    CB: hardening resolve targets

--- test/specs/automated/resolve.spec.js
+++ test/specs/automated/resolve.spec.js
@@ -8,7 +8,7 @@ var opt = {
     srcDir: '../../_loadme'
 };
 
-var loadMeOne = {
+var oneLoadMe = {
     contents: fs.readFileSync(projectPath + '/_loadme/_one.js', 'utf8'),
     r: {
         '_one.js': {
@@ -26,29 +26,38 @@ var loadMeOne = {
     }
 };
 
-var loadMeTwo = {
-    '../../**/*_two*': {
-        name: '../../**/*_two*',
-        dest: projectPath + '/specs/**/*_two*',
-        options: {
-            srcDir: projectPath + '/specs/automated/',
-            outputDir: projectPath + '/specs/automated/bundled'
-        },
-        src: [projectPath + '/_concatme/_two.js',
-            projectPath + '/_loadme/_two.js'
-        ],
-        chain: [],
-        contents: '',
-        sourcemaps: []
-    }
+var twoTwos = {};
+twoTwos[projectPath + '/_concatme/_two.js'] = {
+    name: projectPath + '/_concatme/_two.js',
+    dest: projectPath + '/specs/automated/bundled' + projectPath + '/_concatme/_two.js',
+    options: {
+        srcDir: projectPath + '/specs/automated/',
+        outputDir: projectPath + '/specs/automated/bundled'
+    },
+    src: [projectPath + '/_concatme/_two.js'],
+    chain: [],
+    contents: '',
+    sourcemaps: []
+};
+twoTwos[projectPath + '/_loadme/_two.js'] = {
+    name: projectPath + '/_loadme/_two.js',
+    dest: projectPath + '/specs/automated/bundled' + projectPath + '/_loadme/_two.js',
+    options: {
+        srcDir: projectPath + '/specs/automated/',
+        outputDir: projectPath + '/specs/automated/bundled'
+    },
+    src: [projectPath + '/_loadme/_two.js'],
+    chain: [],
+    contents: '',
+    sourcemaps: []
 };
 
-var loadMeMultiFiles = {
-    '../../_concatme/_one.js': {
-        name: '../../_concatme/_one.js',
-        dest: projectPath + '/specs/_concatme/_one.js',
+var concatMeOneTwo = {
+    '_concatme/_one.js': {
+        name: '_concatme/_one.js',
+        dest: projectPath + '/specs/automated/bundled/_concatme/_one.js',
         options: {
-            srcDir: projectPath + '/specs/automated/',
+            srcDir: projectPath,
             outputDir: projectPath + '/specs/automated/bundled'
         },
         src: [projectPath + '/_concatme/_one.js'],
@@ -56,11 +65,64 @@ var loadMeMultiFiles = {
         contents: '',
         sourcemaps: []
     },
-    '../../_loadme/_two.js': {
-        name: '../../_loadme/_two.js',
-        dest: projectPath + '/specs/_loadme/_two.js',
+    '_concatme/_two.js': {
+        name: '_concatme/_two.js',
+        dest: projectPath + '/specs/automated/bundled/_concatme/_two.js',
         options: {
-            srcDir: projectPath + '/specs/automated/',
+            srcDir: projectPath,
+            outputDir: projectPath + '/specs/automated/bundled'
+        },
+        src: [projectPath + '/_concatme/_two.js'],
+        chain: [],
+        contents: '',
+        sourcemaps: []
+    }
+};
+
+var loadMeOneTwo = {};
+loadMeOneTwo[projectPath + '/_loadme/_one.js'] = {
+    name: projectPath + '/_loadme/_one.js',
+    dest: projectPath + '/specs/automated/bundled' + projectPath + '/_loadme/_one.js',
+    options: {
+        srcDir: projectPath + '/specs/automated/',
+        outputDir: projectPath + '/specs/automated/bundled'
+    },
+    src: [projectPath + '/_loadme/_one.js'],
+    chain: [],
+    contents: '',
+    sourcemaps: []
+};
+loadMeOneTwo[projectPath + '/_loadme/_two.js'] = {
+    name: projectPath + '/_loadme/_two.js',
+    dest: projectPath + '/specs/automated/bundled' + projectPath + '/_loadme/_two.js',
+    options: {
+        srcDir: projectPath + '/specs/automated/',
+        outputDir: projectPath + '/specs/automated/bundled'
+    },
+    src: [projectPath + '/_loadme/_two.js'],
+    chain: [],
+    contents: '',
+    sourcemaps: []
+};
+
+var oneTwoAcross = {
+    '_concatme/_one.js': {
+        name: '_concatme/_one.js',
+        dest: projectPath + '/specs/automated/bundled/_concatme/_one.js',
+        options: {
+            srcDir: projectPath + '',
+            outputDir: projectPath + '/specs/automated/bundled'
+        },
+        src: [projectPath + '/_concatme/_one.js'],
+        chain: [],
+        contents: '',
+        sourcemaps: []
+    },
+    '_loadme/_two.js': {
+        name: '_loadme/_two.js',
+        dest: projectPath + '/specs/automated/bundled/_loadme/_two.js',
+        options: {
+            srcDir: projectPath + '',
             outputDir: projectPath + '/specs/automated/bundled'
         },
         src: [projectPath + '/_loadme/_two.js'],
@@ -70,31 +132,51 @@ var loadMeMultiFiles = {
     }
 };
 
-var loadMeMultiGlobs = {
-    '../../**/*_one*': {
-        name: '../../**/*_one*',
-        dest: projectPath + '/specs/**/*_one*',
+var oneByOneTwoByTwo = {
+    '_concatme/_one.js': {
+        name: '_concatme/_one.js',
+        dest: projectPath + '/specs/automated/bundled/_concatme/_one.js',
         options: {
-            srcDir: projectPath + '/specs/automated/',
+            srcDir: projectPath + '',
             outputDir: projectPath + '/specs/automated/bundled'
         },
-        src: [projectPath + '/_concatme/_one.js',
-            projectPath + '/_loadme/_one.js'
-        ],
+        src: [projectPath + '/_concatme/_one.js'],
         chain: [],
         contents: '',
         sourcemaps: []
     },
-    '../../**/*_two*': {
-        name: '../../**/*_two*',
-        dest: projectPath + '/specs/**/*_two*',
+    '_loadme/_one.js': {
+        name: '_loadme/_one.js',
+        dest: projectPath + '/specs/automated/bundled/_loadme/_one.js',
         options: {
-            srcDir: projectPath + '/specs/automated/',
+            srcDir: projectPath + '',
             outputDir: projectPath + '/specs/automated/bundled'
         },
-        src: [projectPath + '/_concatme/_two.js',
-            projectPath + '/_loadme/_two.js'
-        ],
+        src: [projectPath + '/_loadme/_one.js'],
+        chain: [],
+        contents: '',
+        sourcemaps: []
+    },
+    '_concatme/_two.js': {
+        name: '_concatme/_two.js',
+        dest: projectPath + '/specs/automated/bundled/_concatme/_two.js',
+        options: {
+            srcDir: projectPath + '',
+            outputDir: projectPath + '/specs/automated/bundled'
+        },
+        src: [projectPath + '/_concatme/_two.js'],
+        chain: [],
+        contents: '',
+        sourcemaps: []
+    },
+    '_loadme/_two.js': {
+        name: '_loadme/_two.js',
+        dest: projectPath + '/specs/automated/bundled/_loadme/_two.js',
+        options: {
+            srcDir: projectPath + '',
+            outputDir: projectPath + '/specs/automated/bundled'
+        },
+        src: [projectPath + '/_loadme/_two.js'],
         chain: [],
         contents: '',
         sourcemaps: []
@@ -116,49 +198,41 @@ describe('resolve', function () {
         expect(bundl({}).getResources()).toBe({});
     });
 
-    describe('handles string file', function (expect) {
+    describe('handles string relative file', function (expect) {
         var r = bundl('_one.js', opt).getResources();
-        expect(r).toBe(loadMeOne.r);
-        expect(fs.readFileSync(r['_one.js'].src[0], 'utf8')).toBe(loadMeOne.contents);
+        expect(r).toBe(oneLoadMe.r);
+        expect(fs.readFileSync(r['_one.js'].src[0], 'utf8')).toBe(oneLoadMe.contents);
+    });
+
+    describe('handles string absolute file', function (expect) {
+        expect(bundl(path.resolve('./test/_loadme/_one.js'), opt).getResources()).toBe(oneLoadMe.r);
     });
 
     describe('handles string dir', function (expect) {
-        expect(bundl(path.resolve('./test/_loadme')).getSrcFiles().length).toBe(2, 'absolute');
-        expect(bundl('../../_loadme/').getSrcFiles().length).toBe(2, 'relative');
+        expect(bundl(path.resolve('./test/_loadme')).getResources()).toBe(loadMeOneTwo);
+        expect(bundl('../../_loadme/').getResources()).toBe(loadMeOneTwo);
     });
 
     describe('handles string glob', function (expect) {
         var b = bundl('../../**/*_two*');
-        expect(b.getResources()).toBe(loadMeTwo);
+        expect(b.getResources()).toBe(twoTwos);
         expect(b.getSrcFiles().length).toBe(2);
     });
 
     describe('handles array of one file', function (expect) {
-        expect(bundl(['_one.js'], opt).getResources()).toBe(loadMeOne.r);
+        expect(bundl(['_one.js'], opt).getResources()).toBe(oneLoadMe.r);
     });
 
     describe('handles array of multiple files', function (expect) {
-        expect(bundl(['../../_concatme/_one.js', '../../_loadme/_two.js']).getResources()).toBe(loadMeMultiFiles);
-    });
-
-    describe('handles different types of files', function (expect, done) {
-        var list = [];
-        bundl(['../../_concatme/_one.js', '../../_concatme/_three.html', '../../_concatme/_two.js'])
-            .then({
-                one: function (contents) {
-                    list.push(contents);
-                    return contents;
-                }
-            })
-            .all(function () {
-                expect(list.length).toBe(3);
-                expect(list[1]).toBe('<h1>THREE</h1>\n');
-                done();
-            });
+        expect(bundl(['./_concatme/_one.js', './_loadme/_two.js'], { srcDir: '../../' }).getResources()).toBe(oneTwoAcross);
     });
 
     describe('handles array of multiple globs', function (expect) {
-        expect(bundl(['../../**/*_one*', '../../**/*_two*']).getResources()).toBe(loadMeMultiGlobs)
+        expect(bundl(['**/*_one*', '**/*_two*'], { srcDir: '../../' }).getResources()).toBe(oneByOneTwoByTwo);
+    });
+
+    describe('handles array with ignores', function (expect) {
+        expect(bundl(['./_concatme/**', '!./**/*.html'], { srcDir: '../../' }).getResources()).toBe(concatMeOneTwo);
     });
 
     describe('handles object with files', function (expect) {
@@ -191,6 +265,22 @@ describe('resolve', function () {
             'foo': { contents: 'blah' }
         });
         expect(b.getResources().foo.src[0].contents).toBe('blah');
+    });
+
+    describe('handles different types of files', function (expect, done) {
+        var list = [];
+        bundl(['../../_concatme/_one.js', '../../_concatme/_three.html', '../../_concatme/_two.js'])
+            .then({
+                one: function (contents) {
+                    list.push(contents);
+                    return contents;
+                }
+            })
+            .all(function () {
+                expect(list.length).toBe(3);
+                expect(list[1]).toBe('<h1>THREE</h1>\n');
+                done();
+            });
     });
 
 });
