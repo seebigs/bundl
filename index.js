4371527e7e42ff8656bed61de7371c96567ad4bc
Chris Bigelow <seebigs@gmail.com>
2017-03-10 00:29:19 -0500

    CB: add .shell

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  */
 
 var BundlInstance = require('./lib/instance.js');
+var execa = require('execa');
 var glob = require('glob');
 var Log = require('./lib/log.js');
 var taskman = require('./lib/taskman.js');
@@ -43,6 +44,14 @@ function load (loadPath) {
     taskman.runFromCLI();
 }
 
+function shell (cmd, args, opts) {
+    opts = opts || { reject: false };
+    var exec = execa(cmd, args, opts);
+    exec.stdout.pipe(process.stdout);
+    exec.stderr.pipe(process.stderr);
+    return exec;
+}
+
 
 // Don't rename this function (see discoverRelativePath)
 function bundlModule (targets, options, label) {
@@ -54,8 +63,8 @@ function bundlModule (targets, options, label) {
 bundlModule.args = args;
 bundlModule.load = load;
 bundlModule.run = taskman.run;
-bundlModule.webserver = new BundlInstance().webserver;
+bundlModule.shell = shell;
 bundlModule.task = taskman.task;
-
+bundlModule.webserver = new BundlInstance().webserver;
 
 module.exports = bundlModule;
